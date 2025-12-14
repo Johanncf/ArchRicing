b02f753e5267b2cc8acf8647ff767edeb80b04bd
Johann Figueiredo <johanncfsilva@gmail.com>
2025-12-14 20:52:12 -0300

    init: cloning current configs

---
+++ Configs/hypr/hypr_ricing.sh
@@ -0,0 +1,8 @@
+
+sudo pacman -S hyprland
+sudo pacman -S waybar 
+sudo pacman -S pavucontrol # waybar sound mixer
+sudo pacman -S rofi # app launcher
+sudo pacman -S otf-font-awesome # waybar icons
+sudo pacman -S swaync # notification daemon
+sudo pacman -S hyprshot 
